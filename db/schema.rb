a106413a6ece1e2957fdf50bcb9ccac5c4bffd7b
Fredrik Bergljung <fredrik.bergljung@valtech.se>
2012-12-23 21:54:52 +0100

    When creating tickets, attribute them to the creator.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121223201139) do
+ActiveRecord::Schema.define(:version => 20121223203259) do
 
   create_table "projects", :force => true do |t|
     t.string   "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(:version => 20121223201139) do
     t.integer  "project_id"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.integer  "user_id"
   end
 
   add_index "tickets", ["project_id"], :name => "index_tickets_on_project_id"
