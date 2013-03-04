0ff2458d0c06dbc384904e36539ce3081313955f
Fredrik Bergljung <fredrik.bergljung@valtech.se>
2013-03-04 14:57:47 +0100

    Fix signing up feature, take user to alternative confirmation page

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,8 +1,13 @@
 class Project < ActiveRecord::Base
 	attr_accessible :name
 	has_many :tickets, :dependent => :delete_all
-  	validates :name, :presence => true, :uniqueness => true
+  validates :name, :presence => true, :uniqueness => true
 
-  	has_many :permissions, :as => :thing
-  	scope :readable_by, lambda { |user| joins(:permissions).where(:permissions => { :action => "view", :user_id => user.id})}
+  has_many :permissions, :as => :thing
+  scope :readable_by, lambda { |user| joins(:permissions).where(:permissions => { :action => "view", :user_id => user.id})}
+
+  def self.for(user)
+    user.admin? ? Project : Project.readable_by(user)
+  end
+  
 end
